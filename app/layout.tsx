c3523fcceda659133cd8c19fbefca0b850b4bd03
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-09-21 07:10:39 +0800

    qucik commit - changed the favicon test

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+                <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
