d60cefffe257e3e5799305b4ca721af9532e105a
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-09-20 17:53:46 +0800

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <main>
+                <h1>About</h1>
+            </main>
+        </div>
+    );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+        <div>
+            <main>
+                <h1>Projects</h1>
+            </main>
+        </div>
+    );
+}
